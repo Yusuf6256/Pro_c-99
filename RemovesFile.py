9656104f62a27e9632cd68c46043e6965ddcbd50
Yusuf6256 <78488257+Yusuf6256@users.noreply.github.com>
2022-01-30 16:32:56 +0530

    Add files via upload

---
+++ RemovesFile.py
@@ -0,0 +1,17 @@
+from stat import ST_CTIME
+import time
+import os
+import shutil
+
+path = input("Enter the designated path: ")
+days = int(input("Enter the number of days: "))
+days = time.time()
+
+os.path.exists(path)
+if(os.path.exists(path)==True):
+    os.walk(path)
+
+os.path.join(path)
+ctime = os.stat(path).st_ctime
+os.remove(path)
+shutil.rmtree(path)
